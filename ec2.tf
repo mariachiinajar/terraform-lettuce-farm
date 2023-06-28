a3f240a0150d7779f47bceff216397cef79af73f
Mariachi in a Jar <mariachiinajar@gmail.com>
2023-06-28 11:51:25 +0900

    Configured EC2 on VPC

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,95 @@
-resource "aws_instance" "app_server" {
-  ami           = "ami-022e1a32d3f742bd8"
-  instance_type = "t2.micro"
+data "aws_ami" "amazonlinux" {
+  most_recent = true
 
-  tags = {
-    Name = "AppServer"
+  # search the image with the following conditions
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*"]
+  }
+
+  filter {
+    # type of AMI (ie: hvm or paravirtual).
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
+resource "aws_security_group" "public" {
+  name        = "public"
+  description = "allows public traffic"
+  vpc_id      = aws_vpc.main.id
+
+  # Exclusively allow ssh traffic
+  # only from your machine. 
+  ingress {
+    description = "ssh from local machine"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["211.237.245.156/32"]
+  }
+
+  # traffic goes out to the internet, to all clients. 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "public" }
+}
+
+resource "aws_instance" "public" {
+  count = length(var.public_subnet_cidr_blocks)
+
+  ami                         = data.aws_ami.amazonlinux.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public[count.index].id
+  vpc_security_group_ids      = [aws_security_group.public.id]
+  associate_public_ip_address = true
+  key_name                    = "greenhouse"
+  # putin_khuylo                = true
+
+  tags = { Name = "PublicAppServer-${count.index}" }
+}
+
+resource "aws_security_group" "private" {
+  name        = "private"
+  description = "Allows traffic within VPC"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "private" }
+}
+
+resource "aws_instance" "private" {
+  count                       = length(var.private_subnet_cidr_blocks)
+  ami                         = data.aws_ami.amazonlinux.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.private[count.index].id
+  vpc_security_group_ids      = [aws_security_group.private.id]
+  associate_public_ip_address = false
+  key_name                    = "greenhouse"
+  # putin_khuylo                = true
+
+  tags = { Name = "private" }
+}
+
+output "public_ip_address" {
+  value = aws_instance.public[*].public_ip
+}
--- vpc.tf
+++ vpc.tf
@@ -1,116 +1,116 @@
 # VPC
 resource "aws_vpc" "main" {
-    cidr_block = var.vpc_cidr_block
+  cidr_block = var.vpc_cidr_block
 
-    tags = {
-        Name = "greenhouse"
-    }
+  tags = {
+    Name = "greenhouse"
+  }
 }
 
 # Subnets
 resource "aws_subnet" "public" {
-    count = length(var.public_subnet_cidr_blocks)
+  count = length(var.public_subnet_cidr_blocks)
 
-    vpc_id            = aws_vpc.main.id
-    cidr_block        = var.public_subnet_cidr_blocks[count.index]
-    availability_zone = var.availability_zones[count.index]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidr_blocks[count.index]
+  availability_zone = var.availability_zones[count.index]
 
-    map_public_ip_on_launch = true
+  map_public_ip_on_launch = true
 
-    tags = {
-        Name = "public"
-    }
+  tags = {
+    Name = "public"
+  }
 }
 
 resource "aws_subnet" "private" {
-    count = length(var.private_subnet_cidr_blocks)
+  count = length(var.private_subnet_cidr_blocks)
 
-    vpc_id            = aws_vpc.main.id
-    cidr_block        = var.private_subnet_cidr_blocks[count.index]
-    availability_zone = var.availability_zones[count.index]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr_blocks[count.index]
+  availability_zone = var.availability_zones[count.index]
 
-    map_public_ip_on_launch = false
+  map_public_ip_on_launch = false
 
-    tags = {
-        Name = "private"
-    }
+  tags = {
+    Name = "private"
+  }
 }
 
 # Internet connection for public subnets
 resource "aws_internet_gateway" "main" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-    tags = {
-        Name = "internet gw"
-    }
+  tags = {
+    Name = "internet gw"
+  }
 }
 
 # Internet connection for private subnets
 resource "aws_eip" "nat" {
-    count = length(var.public_subnet_cidr_blocks)
-    vpc   = true
+  count = length(var.public_subnet_cidr_blocks)
+  vpc   = true
 
-    tags = {
-        Name = "elastic-IP-${count.index}"
-    }
+  tags = {
+    Name = "elastic-IP-${count.index}"
+  }
 }
 
 resource "aws_nat_gateway" "main" {
-    count = length(var.public_subnet_cidr_blocks)
+  count = length(var.public_subnet_cidr_blocks)
 
-    allocation_id = aws_eip.nat[count.index].id
-    subnet_id     = aws_subnet.public[count.index].id
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
 
-    tags = {
-        Name = "nat-gw-${count.index}"
-    }
+  tags = {
+    Name = "nat-gw-${count.index}"
+  }
 }
 
 # Routing traffic
 resource "aws_route_table" "public" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-    # traffic from public subnet
-    # goes out to the internet through Internet GW.
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.main.id
-    }
+  # traffic from public subnet
+  # goes out to the internet through Internet GW.
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
 
-    tags = {
-        Name = "public"
-    }
+  tags = {
+    Name = "public"
+  }
 }
 
 resource "aws_route_table" "private" {
-    count = length(var.private_subnet_cidr_blocks)
+  count = length(var.private_subnet_cidr_blocks)
 
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
-    # traffic from private subnet
-    # goes out to the internet 
-    # via NAT GW attached to public subnet.
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_nat_gateway.main[count.index].id
-    }
+  # traffic from private subnet
+  # goes out to the internet 
+  # via NAT GW attached to public subnet.
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.main[count.index].id
+  }
 
-    tags = {
-        Name = "private-${count.index}"
-    }
+  tags = {
+    Name = "private-${count.index}"
+  }
 }
 
 resource "aws_route_table_association" "public" {
-    count = length(var.public_subnet_cidr_blocks)
+  count = length(var.public_subnet_cidr_blocks)
 
-    subnet_id      = aws_subnet.public[count.index].id
-    route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
-    count = length(var.private_subnet_cidr_blocks)
+  count = length(var.private_subnet_cidr_blocks)
 
-    subnet_id      = aws_subnet.private[count.index].id
-    route_table_id = aws_route_table.private[count.index].id
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
 
